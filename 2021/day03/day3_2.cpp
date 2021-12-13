2aec356ad00870ae0366284583dbbf897261df45
Jules van der Hoek <julesvanderhoek@Juless-MacBook-Air.local>
2021-12-13 15:12:03 +0100

    day3part2

--- 2021/day03/day3_2.cpp
+++ 2021/day03/day3_2.cpp
@@ -6,46 +6,141 @@
 /*   By: juvan-de <juvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/03 13:44:23 by juvan-de      #+#    #+#                 */
-/*   Updated: 2021/12/07 12:50:42 by juvan-de      ########   odam.nl         */
+/*   Updated: 2021/12/13 15:11:30 by julesvander   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <math.h>
 #include <vector>
 
-int	count_ones()
+std::vector<int>	parse_input(std::string input)
 {
-	if (in[i
-	])
+	std::vector<int>	res;
+	int i = 0;
+
+	while (input[i])
+	{
+		res.push_back(input[i] - '0');
+		i++;
+	}
+	return (res);
+}
+
+void	print_array(std::vector<std::vector<int> > array)
+{
+	for (int i = 0; i < array.size(); i++)
+	{
+		for (int j = 0; j < array[i].size(); j++)
+		{
+			std::cout << array[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int		find_most_common(int index, std::vector<std::vector<int> > & array)
+{
+	int	ones = 0;
+	int	zeroes = 0;
+	for (int i = 0; i < array.size(); i++)
+	{
+		if (array[i][index] == 1)
+			ones++;
+		else if (array[i][index] == 0)
+			zeroes++;
+	}
+	if (zeroes <= ones)
+		return (1);
+	return (0);
+}
+
+int		find_least_common(int index, std::vector<std::vector<int> > & array)
+{
+	int	ones = 0;
+	int	zeroes = 0;
+	for (int i = 0; i < array.size(); i++)
+	{
+		if (array[i][index] == 1)
+			ones++;
+		else if (array[i][index] == 0)
+			zeroes++;
+	}
+	if (zeroes > ones)
+		return (1);
+	return (0);
+}
+
+void	delete_uncommons(int ref, std::vector<std::vector<int> > & array, int index)
+{
+	int	len = array.size();
+	for (int i = 0; i < len; i++)
+	{
+		if (ref != array[i][index])
+		{
+			if (array.size() > 1)
+			{
+				array.erase(array.begin() + i);
+				len--;
+				i--;
+			}
+			else
+				return ;
+		}
+	}
+}
+
+void	empty_array(std::vector<std::vector<int> > & array)
+{
+	while (array.size() > 0)
+	{
+		array.erase(array.begin());
+	}
 }
 
 int		main(void)
 {
-	std::string			buffer;
-	std::ifstream		input;
-	size_t				remaining = 2998;
-	bool				in[2998];
-
-	for (int i = 0; i < 2998; i++)
-	{
-		in[i] = true;
-	}
+	std::string	buffer;
+	std::ifstream	input;
+	std::ifstream	input2;
 	input.open("input.txt");
-	int		count[12];
-	int		out[12];
+	input2.open("input.txt");
+	std::vector<std::vector<int> >	array;
+	int					most_common;
+	int					least_common;
 	while (getline(input, buffer))
 	{
-		for (int i = 0; i < 12; i++)
-		{
-			count[i] += buffer[i] - '0';
-		}
+		array.push_back(parse_input(buffer));
 	}
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < array[0].size(); i++)
 	{
-		if (count[i] >= remaining / 2)
+		most_common = find_most_common(i, array);
+		delete_uncommons(most_common, array, i);
+		if (array.size() <= 1)
+			break ;
 	}
+	for (int i = 0;i < array[0].size(); i++)
+		std::cout << array[0][i];
+	std::cout << std::endl;
+	empty_array(array);
+
+	while (getline(input2, buffer))
+	{
+		array.push_back(parse_input(buffer));
+	}
+	for (int i = 0; i < array[0].size(); i++)
+	{
+		least_common = find_least_common(i, array);
+		delete_uncommons(least_common, array, i);
+		if (array.size() <= 1)
+			break ;
+	}
+	for (int i = 0;i < array[0].size(); i++)
+		std::cout << array[0][i];
+	std::cout << std::endl;
+
+	std::cout << 673 * 2397 << std::endl;
 	return (0);
 }
